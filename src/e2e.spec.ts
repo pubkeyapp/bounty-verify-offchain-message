34155340ab6a2ef36ac913c2ec40caee7b962221
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2024-03-06 02:06:54 +0530

    feat: complete bounty requirements

--- src/e2e.spec.ts
+++ src/e2e.spec.ts
@@ -1,25 +1,30 @@
-import { Keypair } from '@solana/web3.js'
-import { beforeAll, expect } from 'vitest'
-import { testKeypair } from './fixtures/test-keypair'
+import * as ed25519 from '@noble/ed25519'
+import * as bs58 from 'bs58'
+
+import { expect } from 'vitest'
 import { createChallenge } from './lib/create-challenge'
-import { verifySignature } from './lib/verify-signature'
+import { constructSolanaMessage, verifySignature } from './lib/verify-signature'
 
 describe('e2e', () => {
-  let keypair: Keypair
-
-  beforeAll(async () => {
-    keypair = await testKeypair
-  })
-
-  it('should be implemented', () => {
+  it('should be implemented', async () => {
     // ARRANGE
-    // Use createChallenge
+    const message = 'Test Message'
+    const privateKey = ed25519.utils.randomPrivateKey()
+    const publicKeyBytes = await ed25519.getPublicKey(privateKey)
+    const publicKey = bs58.encode(publicKeyBytes)
+    const challenge = createChallenge({ message, publicKey })
+
+    // Signing the message
+    const messageBytes = constructSolanaMessage(challenge)
+    const signatureBytes = await ed25519.sign(messageBytes, privateKey)
+    const signature = bs58.encode(signatureBytes)
 
     // ACT
-    // Use verifySignature
+    const isValid = await verifySignature({ challenge, signature, publicKey })
 
     // ASSERT
     expect(createChallenge).toBeDefined()
     expect(verifySignature).toBeDefined()
+    expect(isValid).toBeTruthy()
   })
 })
--- src/lib/verify-signature.ts
+++ src/lib/verify-signature.ts
@@ -1,5 +1,38 @@
-export function verifySignature(input: { challenge: string; signature: string; publicKey: string }): boolean {
-  // TODO: Implement signature verification
+import * as ed25519 from '@noble/ed25519'
+import * as bs58 from 'bs58'
 
-  return false
+export async function verifySignature({
+  challenge,
+  signature,
+  publicKey,
+}: {
+  challenge: string
+  signature: string
+  publicKey: string
+}) {
+  const messageBytes = constructSolanaMessage(challenge)
+  const publicKeyBytes = bs58.decode(publicKey)
+  const signatureBytes = bs58.decode(signature)
+
+  return ed25519.verify(signatureBytes, messageBytes, publicKeyBytes)
+}
+
+export function constructSolanaMessage(message: string): Uint8Array {
+  const escapeSequenceFF = new Uint8Array([255])
+  const signingDomain = new TextEncoder().encode('solana offchain')
+  const headerVersion = new Uint8Array([0])
+  const headerFormat = new Uint8Array([0])
+  const messageLength = new Uint8Array([64, 0])
+  const messageBytes = new TextEncoder().encode(message)
+
+  const payload = new Uint8Array([
+    ...escapeSequenceFF,
+    ...signingDomain,
+    ...headerVersion,
+    ...headerFormat,
+    ...messageLength,
+    ...messageBytes,
+  ])
+
+  return payload
 }
